803f61173f16f320944426fc5466d64b46e4cbfe
alextomy <alextomy8@gmail.com>
2025-06-18 10:43:17 +0530

    feat: changes to execute on firefox browser

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
-chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ['rotate.js', 'revert.js']
-  });
+// Use the browser API for Firefox
+browser.browserAction.onClicked.addListener((tab) => {
+  // Fallback to tabs.executeScript for Firefox
+  browser.tabs.executeScript(tab.id, { file: 'rotate.js' });
+  browser.tabs.executeScript(tab.id, { file: 'revert.js' });
 });
